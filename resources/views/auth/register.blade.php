9d75fc52eccf79ed358006a9976ad990ec7d8966
Renthop <44197289+Renth0p@users.noreply.github.com>
2022-04-20 22:23:14 +0300

    Обновлен файл регистрации и его дизайн

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
     <div class="striped-container">
         <form action="{{ route("register_process") }}" method="post" class="reg_from">
             @csrf
-            <h1>Sign up</h1>
+            <h1>Creating your account</h1>
             <p class="striped__subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci aliquam animi assumenda beatae ea eius eveniet, expedita explicabo facere fugiat illo magnam, nihil numquam pariatur quibusdam reiciendis reprehenderit sapiente.</p>
             <input type="text" name="name" placeholder="Login" class="border-gray-800 @error('name') border-red-500 @enderror"><br>
 
@@ -22,7 +22,7 @@
             @error('password_confirmation')
             <p class="text-red-500">{{ $message }}</p>
             @enderror
-            <button type="submit">Create account</button>
+            <button type="submit">Sign in</button>
             <div class="forget_pass">
                 <p>
                     You have already account? - <a href="{{route("login")}}">Sign in!</a>
